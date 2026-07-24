40e4dad55b97c01b58cdc4006b4cd2af85962a29
00660 <00660@users.noreply.github.com>
2026-07-24 19:07:03 +0000

    Limit voice segments below WeChat duration

--- app/src/main/java/com/vxbot/wechatbot/VoiceReply.java
+++ app/src/main/java/com/vxbot/wechatbot/VoiceReply.java
@@ -17,9 +17,9 @@ import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 final class VoiceReply {
-    private static final int VOICE_SEGMENT_TARGET_MS = 52000;
-    private static final int VOICE_SEGMENT_MAX_MS = 55000;
-    private static final int VOICE_SEGMENT_RETRY_TARGET_MS = 50000;
+    private static final int VOICE_SEGMENT_TARGET_MS = 48000;
+    private static final int VOICE_SEGMENT_MAX_MS = 40000;
+    private static final int VOICE_SEGMENT_RETRY_TARGET_MS = 37000;
     private static final int VOICE_SEGMENT_GAP_MS = 800;
     private static final int PRE_PLAYBACK_PRESS_MS = 500;
 
